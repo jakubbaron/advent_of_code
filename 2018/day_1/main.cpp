52d29f63a8511611299ac722d26f9a23be70dcb4
Jakub Baron <jakubaron@gmail.com>
2018-12-03 20:08:47 +0000

    Day 1, part two, frequency two twice@

--- 2018/day_1/main.cpp
+++ 2018/day_1/main.cpp
@@ -2,24 +2,38 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <set>
 
 
 int main(int argc, char** argv) {
   std::ifstream infile("input.txt");
   std::string line;
   long frequency{0}, last_frequency{0};
-  while (std::getline(infile, line))
-  {
-    std::istringstream iss(line);
-    int change{0};
-    if (!(iss >> change)) { 
-      std::cerr << "Coudln't get line, finishing";
-      break;
-    } // error
-    last_frequency = frequency;
-    frequency += change;
-    std::cout << "Current frequency " << last_frequency << ", change of " << change << "; resulting frequency " << frequency << ".\n";
+  std::set<long> frequencies;
+  bool found{false};
+  while(!found) {
+    std::ifstream infile("input.txt");
+    while (std::getline(infile, line))
+    {
+      std::istringstream iss(line);
+      int change{0};
+      if (!(iss >> change)) { 
+        std::cerr << "Coudln't get line, finishing";
+        break;
+      } // error
+      last_frequency = frequency;
+      frequency += change;
+      if(frequencies.count(frequency)) { 
+        std::cout << "reached frequency twice: " << frequency << std::endl;
+        found = true;
+        break;
+      } else {
+        frequencies.insert(frequency);
+      }
+      std::cout << "Current frequency " << last_frequency << ", change of " << change << "; resulting frequency " << frequency << ".\n";
+    }
   }
   std::cout << "Result frequency: " << frequency << std::endl;
+  std::cout << "Set count: " << frequencies.size() << std::endl; 
   return 0;
 }
