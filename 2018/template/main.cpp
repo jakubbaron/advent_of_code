3de04565a10ccb580e2536fea0b70c8094d0a1a7
Jakub Baron <jakubaron@gmail.com>
2018-12-05 13:13:27 +0000

    Removing istringstream from template as in most cases it's not used so far

--- 2018/template/main.cpp
+++ 2018/template/main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 
 int main(int argc, char** argv) {
   std::ifstream infile("input.txt");
   std::string line;
   while (std::getline(infile, line)) {
-    std::istringstream iss(line);
-    int change{0};
-    if (!(iss >> change)) { 
-      std::cerr << "Coudln't get line, finishing";
-      break;
-    } // error
+
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
