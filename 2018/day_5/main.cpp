9235ebb3b3f7964c172f93f2005d913f9c30cbe0
Jakub Baron <jakubaron@gmail.com>
2018-12-05 13:12:45 +0000

    Day 5, polymer after removing one of the specific characters, improved polymer:)

--- 2018/day_5/main.cpp
+++ 2018/day_5/main.cpp
@@ -2,23 +2,23 @@
 #include <fstream>
 #include <stack>
 #include <algorithm>
+#include <cctype>
+#include <set>
 
-int main(int argc, char** argv) {
-  std::ifstream infile("input.txt");
-  std::string line;
+auto react_polymer(const std::string& line, char skip_char = '\0') {
   constexpr auto polarity = 'a' - 'A';
-  std::getline(infile, line); //no while loop, we expect only one line;
-  if(line.empty()) {
-    std::cerr << "Empty file" << std::endl;
-    return EXIT_FAILURE;
-  }
   std::stack<char> polymer;
   for(int i = 0; i < line.size(); i++) {
     char current_char = line[i];
+    if(std::toupper(current_char) == std::toupper(skip_char)) {
+      continue;
+    }
+
     if(polymer.empty()) {
       polymer.push(current_char);
       continue;
     }
+
     char top_char = polymer.top();
 
     if(std::abs(top_char - current_char) == polarity) {
@@ -27,7 +27,24 @@ int main(int argc, char** argv) {
       polymer.push(current_char);
     }
   }
-  std::cout << "Left out elements: " << polymer.size() << " from: " << line.size() << std::endl;
+  return polymer.size();
+}
+
+int main(int argc, char** argv) {
+  std::ifstream infile("input.txt");
+  std::string line;
+  std::getline(infile, line); //no while loop, we expect only one line;
+  if(line.empty()) {
+    std::cerr << "Empty file" << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::cout << "Left out elements: " << react_polymer(line) << " from: " << line.size() << std::endl;
+
+  std::set<size_t> polymer_lengths;
+  for(char remove_char = 'a'; remove_char <= 'z'; remove_char++) {
+    polymer_lengths.insert(react_polymer(line, remove_char)); 
+  }
+  std::cout << "The shortest polymer length: " << *polymer_lengths.begin() << std::endl;
 
   return EXIT_SUCCESS;
 }
