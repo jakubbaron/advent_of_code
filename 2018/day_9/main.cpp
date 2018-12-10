e9d5a1f048001b800d67b84dd6738f904bc69bd2
Jakub Baron <jakubaron@gmail.com>
2018-12-10 00:10:56 +0000

    Day 9, init

---
+++ 2018/day_9/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv) {
+  std::ifstream infile("input.txt");
+  std::string line;
+  while (std::getline(infile, line)) {
+
+  }
+
+  return EXIT_SUCCESS;
+}
