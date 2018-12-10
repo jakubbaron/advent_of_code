a05a3cd7b1f0a8bc57d8dd46a7007701cf65be06
Jakub Baron <jakubaron@gmail.com>
2018-12-10 18:52:01 +0000

    Day 9, part 2, 100, improved

--- 2018/day_9/main.cpp
+++ 2018/day_9/main.cpp
@@ -1,12 +1,62 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+//459 players; last marble is worth 71790 points
+
+class Game { 
+  public:
+    Game(int players, int last_marble):
+      m_players_no(players),
+      m_last_marble(last_marble) {
+
+      scores.resize(m_players_no);
+      game_board.push_back(0);
+    }
+
+    void play_game() {
+      auto current_iterator = game_board.cbegin();
+      for(auto marble = 1, player = 0;
+          marble != m_last_marble + 1;
+          player = (player + 1 ) % m_players_no,
+          marble++) {
+        if(marble % 23) {
+          if(++current_iterator == game_board.cend()) {
+            current_iterator = game_board.cbegin();
+          }
+          current_iterator = game_board.insert(++current_iterator, marble);
+        } else {
+            for (auto i = 0; i != 7; ++i, --current_iterator) {
+              if(current_iterator == game_board.cbegin()) {
+                current_iterator = game_board.cend();
+              }
+            }
+            scores[player] += marble + *current_iterator;
+            game_board.erase(current_iterator++);
+        }
+      }
+    }
+
+    void print_scores() const {
+      auto max_el = std::max_element(scores.begin(), scores.end());
+      std::cout << "Highest score: " << *max_el << std::endl;
+    }
+
+  private: 
+    int m_players_no;
+    int m_last_marble;
+    std::vector<long> scores;
+    std::list<long> game_board; 
+};
 
 int main(int argc, char** argv) {
-  std::ifstream infile("input.txt");
-  std::string line;
-  while (std::getline(infile, line)) {
+  constexpr int players_no = 459;
+  constexpr int last_marble = 71790 * 100;
 
-  }
+  Game g(players_no, last_marble);
+  g.play_game();
+  g.print_scores();
 
   return EXIT_SUCCESS;
 }
