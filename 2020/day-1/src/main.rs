7a6df6b4f48ad8bd34c5087901e3052744307723
Jakub Baron <jakubaron@gmail.com>
2020-12-03 18:36:00 +0100

    2020, day 2 + small refactor

--- 2020/day-1/src/main.rs
+++ 2020/day-1/src/main.rs
@@ -6,16 +6,32 @@ use std::collections::HashSet;
 fn main() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let f = BufReader::new(f);
+    let mut vec = Vec::new();
     let mut numbers = HashSet::new();
 
     for line in f.lines() {
         let my_string = line.unwrap();
         let my_int = my_string.parse::<i32>().unwrap();
-        if numbers.contains(&my_int) {
+        vec.push(my_int);
+    }
+
+    for my_int in &vec {
+        if numbers.contains(my_int) {
             println!("{}", my_int * (2020 - my_int));
-            return Ok(());
+            break;
         }
         numbers.insert(2020 - my_int);
+    }
+
+    for int1 in &vec {
+        for int2 in &vec {
+            for int3 in &vec {
+                if int1 + int2 + int3 == 2020 {
+                    println!("{}", int1 * int2 * int3);
+                    return Ok(());
+                }
+            }
+        }
     }
 
     Ok(())
