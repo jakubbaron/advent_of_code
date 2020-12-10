968284c398b17de82aba12a7a144a44894b8d134
Jakub Baron <jakubaron@gmail.com>
2020-12-10 09:47:34 +0100

    Day 10, part two powers

--- 2020/day-10/src/main.rs
+++ 2020/day-10/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, BufReader};
@@ -16,19 +17,38 @@ fn main() -> io::Result<()> {
     }
     let mut vec: Vec<i32> = vec.into_iter().map(|x| x.parse::<i32>().unwrap()).collect();
     vec.sort();
+    vec.insert(0, 0);
+    vec.insert(vec.len(), vec.last().unwrap() + 3);
     let vec = vec;
     let mut count: HashMap<i32, i32> = HashMap::new();
-    // println!("{:?}", vec);
-
-    *count.entry(&vec[0] - &0).or_insert(0) += 1;
     for i in 1..vec.len() {
         let diff = &vec[i] - &vec[i-1];
         *count.entry(diff).or_insert(0) += 1;
     }
-    *count.entry(3).or_insert(0) += 1;
-    for (k, v) in count.iter() {
-        println!("{} {}", k, v);
-    }
+    // for (k, v) in count.iter() {
+    //     println!("{} {}", k, v);
+    // }
     println!("{}", count.get(&1).unwrap() * count.get(&3).unwrap());
+
+    // let available: HashSet<i32> = vec.to_vec().into_iter().collect();
+
+    let mut pow2 = 0;
+    let mut pow7 = 0;
+
+    for i in 1..vec.len() - 1 {
+        let negative;
+        if i >= 3 {
+            negative = vec[i-3];
+        } else {
+            negative = -9999;
+        }
+        if vec[i+1] - negative == 4 {
+            pow7 += 1;
+            pow2 -= 2;
+        } else if vec[i+1] - vec[i-1] == 2 {
+            pow2 += 1;
+        }
+    }
+    println!("{}", 2_i64.pow(pow2) * 7_i64.pow(pow7));
     Ok(())
 }
