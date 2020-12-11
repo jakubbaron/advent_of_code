14c3742a8b46b1fe883616976080bd849faebe6b
Jakub Baron <jakubaron@gmail.com>
2020-12-11 10:03:15 +0100

    Day 11, part 2

--- 2020/day-11/src/main.rs
+++ 2020/day-11/src/main.rs
@@ -74,6 +74,68 @@ fn main() -> io::Result<()> {
         if !changed {
             break;
         }
+        // println!("New round");
+        // for v in after.iter() {
+        //     println!("{:?}", v);
+        // }
+        before = after.to_vec();
+    }
+
+    let mut sum = 0;
+    for v in after.iter() {
+        for c in v.iter() {
+            if *c == '#' {
+                sum += 1;
+            }
+        }
+    }
+    println!("Occupied seats: {}", sum);
+
+    let mut after = vec.to_vec();
+    let mut before = vec.to_vec();
+
+    loop {
+        let mut changed = false;
+        for (i, row) in before.iter().enumerate() {
+            for (j, ch) in row.iter().enumerate() {
+                let ch = *ch;
+                if ch == '.' {
+                    continue;
+                }
+                let mut occ_count = 0;
+                let directions = vec![
+                    (-1, -1), (-1, 0), (-1, 1),
+                    (0, -1), (0, 1),
+                    (1, -1), (1, 0), (1, 1)];
+                for dir in directions.iter() {
+                    let (horiz, vert) = dir;
+                    let mut cj = j as i32 + horiz;
+                    let mut ci = i as i32 + vert;
+                    while ci >= 0 && ci < before.len() as i32 && cj >= 0 && cj < row.len() as i32 {
+                        if before[ci as usize][cj as usize] == 'L' {
+                            break;
+                        }
+                        if before[ci as usize][cj as usize] == '#' {
+                            occ_count += 1;
+                            break;
+                        }
+                        cj = cj + horiz;
+                        ci = ci + vert;
+                    }
+                }
+                if ch == '#' && occ_count >= 5 {
+                    after[i][j] = 'L';
+                    changed = true;
+                }
+                if ch == 'L' && occ_count == 0 {
+                    after[i][j] = '#';
+                    changed = true;
+                }
+            }
+        }
+        if !changed {
+            break;
+        }
         println!("New round");
         for v in after.iter() {
             println!("{:?}", v);
