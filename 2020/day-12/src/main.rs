f4676bf147bf4bb9e3606d9d5690a1e4c7e720c2
Jakub Baron <jakubaron@gmail.com>
2020-12-12 16:00:14 +0100

    Refactor to use Point Struct

--- 2020/day-12/src/main.rs
+++ 2020/day-12/src/main.rs
@@ -1,5 +1,3 @@
-// use std::collections::HashMap;
-// use std::collections::HashSet;
 use regex::Regex;
 use std::io::{self};
 
@@ -7,6 +5,32 @@ use std::io::{self};
 struct Instruction {
     letter: String,
     number: i32,
+}
+
+#[derive(Debug, Clone)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn add(&mut self, other: &Point) {
+        self.x += other.x;
+        self.y += other.y;
+    }
+
+    fn abs(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+
+    fn rotate(&mut self, angle: f32) {
+        let s = angle.to_radians().sin().round();
+        let c = angle.to_radians().cos().round();
+        let ynew = self.y as f32 * c - self.x as f32 * s;
+        let xnew = self.y as f32 * s + self.x as f32 * c;
+        self.x = xnew as i32;
+        self.y = ynew as i32;
+    }
 }
 
 fn modulo(x: i32, m: i32) -> i32 {
@@ -39,96 +63,68 @@ fn main() -> io::Result<()> {
     let instructions: Vec<Instruction> = instructions;
     let directions = vec!["N", "E", "S", "W"];
     let mut dir_idx = 1_i32;
-    let mut pos = (0_i32, 0_i32);
+    let mut pos = Point { x: 0, y: 0 };
     for instruction in &instructions {
         println!("{:?}", instruction);
         let Instruction { letter, number } = instruction;
         let number = *number;
-        let mut vertical_move = 0;
-        let mut horizontal_move = 0;
-        let mut spin_move = 0;
-        let helper: String;
-        if letter == "F" {
-            helper = directions[dir_idx as usize].to_string();
-        } else {
-            helper = letter.to_string();
-        }
-        match helper.as_str() {
-            "N" => {
-                vertical_move = number;
-            }
-            "S" => {
-                vertical_move = -number;
-            }
-            "E" => {
-                horizontal_move = number;
-            }
-            "W" => {
-                horizontal_move = -number;
-            }
-            "L" => {
-                spin_move = -number / 90;
-            }
-            "R" => {
-                spin_move = number / 90;
-            }
-            _ => (),
-        }
-        pos.0 += vertical_move;
-        pos.1 += horizontal_move;
+        let helper = match letter.as_str() {
+            "F" => directions[dir_idx as usize].to_string(),
+            _ => letter.to_string(),
+        };
+
+        let point = match helper.as_str() {
+            "N" => Point { y: number, x: 0 },
+            "S" => Point { y: -number, x: 0 },
+            "E" => Point { y: 0, x: number },
+            "W" => Point { y: 0, x: -number },
+            _ => Point { x: 0, y: 0 },
+        };
+        pos.add(&point);
+
+        let spin_move = match helper.as_str() {
+            "L" => -number / 90,
+            "R" => number / 90,
+            _ => 0,
+        };
         dir_idx += spin_move % 4;
         dir_idx = modulo(dir_idx, 4);
         println!("Pos {:?} direction {}", pos, directions[dir_idx as usize]);
     }
-    println!("Manhatann distance {}", pos.0.abs() + pos.1.abs());
+    println!("Manhatann distance {}", pos.abs());
+    assert_eq!(pos.abs(), 441);
 
-    let mut waypoint = (1_i32, 10_i32);
-    let mut ship_pos = (0_i32, 0_i32);
+    let mut waypoint = Point { x: 10, y: 1 };
+    let mut ship_pos = Point { x: 0, y: 0 };
     for instruction in &instructions {
         println!("{:?}", instruction);
         let Instruction { letter, number } = instruction;
         let number = *number;
-        let mut w_vertical_move = 0;
-        let mut w_horizontal_move = 0;
-        let mut s_vertical_move = 0;
-        let mut s_horizontal_move = 0;
-        let mut angle = 0_f32;
-        match letter.as_str() {
-            "N" => {
-                w_vertical_move = number;
-            }
-            "S" => {
-                w_vertical_move = -number;
-            }
-            "E" => {
-                w_horizontal_move = number;
-            }
-            "W" => {
-                w_horizontal_move = -number;
-            }
-            "L" => {
-                angle = -number as f32;
-            }
-            "R" => {
-                angle = number as f32;
-            }
-            "F" => {
-                s_vertical_move = number * waypoint.0;
-                s_horizontal_move = number * waypoint.1;
-            }
-            _ => (),
-        }
-        let s = angle.to_radians().sin().round();
-        let c = angle.to_radians().cos().round();
-        let xnew = waypoint.0 as f32 * c - waypoint.1 as f32 * s;
-        let ynew = waypoint.0 as f32 * s + waypoint.1 as f32 * c;
-        waypoint = (xnew as i32, ynew as i32);
-        waypoint.0 += w_vertical_move;
-        waypoint.1 += w_horizontal_move;
-        ship_pos.0 += s_vertical_move;
-        ship_pos.1 += s_horizontal_move;
-        println!("ShipPos {:?} waypoint {:?}", ship_pos, waypoint);
+        let waypoint_move = match letter.as_str() {
+            "N" => Point { y: number, x: 0 },
+            "S" => Point { y: -number, x: 0 },
+            "E" => Point { y: 0, x: number },
+            "W" => Point { y: 0, x: -number },
+            _ => Point { x: 0, y: 0 },
+        };
+        let angle: f32 = match letter.as_str() {
+            "L" => -number,
+            "R" => number,
+            _ => 0,
+        } as f32;
+        let ship_move = match letter.as_str() {
+            "F" => Point {
+                y: number * waypoint.y,
+                x: number * waypoint.x,
+            },
+            _ => Point { x: 0, y: 0 },
+        };
+        waypoint.rotate(angle);
+        waypoint.add(&waypoint_move);
+        ship_pos.add(&ship_move);
+        println!("ShipPos {:?} Waypoint {:?}", ship_pos, waypoint);
     }
-    println!("Manhatann distance {}", ship_pos.0.abs() + ship_pos.1.abs());
+    println!("Manhatann distance {}", ship_pos.abs());
+    assert_eq!(ship_pos.abs(), 40014);
     Ok(())
 }
