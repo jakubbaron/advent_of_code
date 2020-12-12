97a44597f8c939afa92f66bbc2d80a77093bcc06
Jakub Baron <jakubaron@gmail.com>
2020-12-12 11:18:16 +0100

    rustfmt

--- 2020/day-12/src/main.rs
+++ 2020/day-12/src/main.rs
@@ -1,7 +1,7 @@
 // use std::collections::HashMap;
 // use std::collections::HashSet;
-use std::io::{self};
 use regex::Regex;
+use std::io::{self};
 
 #[derive(Debug, Clone)]
 struct Instruction {
@@ -24,25 +24,25 @@ fn main() -> io::Result<()> {
     let re = Regex::new(r"^([WNSEWLRF])(\d+)$").unwrap();
     let mut instructions: Vec<Instruction> = Vec::new();
     for line in vec.into_iter() {
-       if !re.is_match(&line) {
-           println!("NOPE {}", &line);
-       }
-       let caps = re.captures(&line).unwrap();
+        if !re.is_match(&line) {
+            println!("NOPE {}", &line);
+        }
+        let caps = re.captures(&line).unwrap();
         let letter = caps.get(1).map_or("", |m| m.as_str()).to_string();
         let number = caps
             .get(2)
             .map_or("", |m| m.as_str())
             .parse::<i32>()
             .unwrap();
-        instructions.push(Instruction{letter, number});
+        instructions.push(Instruction { letter, number });
     }
-    let instructions:Vec<Instruction> = instructions;
+    let instructions: Vec<Instruction> = instructions;
     let directions = vec!["N", "E", "S", "W"];
     let mut dir_idx = 1_i32;
     let mut pos = (0_i32, 0_i32);
     for instruction in &instructions {
         println!("{:?}", instruction);
-        let Instruction{letter, number} = instruction;
+        let Instruction { letter, number } = instruction;
         let number = *number;
         let mut vertical_move = 0;
         let mut horizontal_move = 0;
@@ -56,23 +56,23 @@ fn main() -> io::Result<()> {
         match helper.as_str() {
             "N" => {
                 vertical_move = number;
-            },
+            }
             "S" => {
-                vertical_move = - number;
-            },
+                vertical_move = -number;
+            }
             "E" => {
                 horizontal_move = number;
-            },
+            }
             "W" => {
                 horizontal_move = -number;
-            },
+            }
             "L" => {
                 spin_move = -number / 90;
-            },
+            }
             "R" => {
                 spin_move = number / 90;
-            },
-            _ => ()
+            }
+            _ => (),
         }
         pos.0 += vertical_move;
         pos.1 += horizontal_move;
@@ -86,7 +86,7 @@ fn main() -> io::Result<()> {
     let mut ship_pos = (0_i32, 0_i32);
     for instruction in &instructions {
         println!("{:?}", instruction);
-        let Instruction{letter, number} = instruction;
+        let Instruction { letter, number } = instruction;
         let number = *number;
         let mut w_vertical_move = 0;
         let mut w_horizontal_move = 0;
@@ -96,27 +96,27 @@ fn main() -> io::Result<()> {
         match letter.as_str() {
             "N" => {
                 w_vertical_move = number;
-            },
+            }
             "S" => {
-                w_vertical_move = - number;
-            },
+                w_vertical_move = -number;
+            }
             "E" => {
                 w_horizontal_move = number;
-            },
+            }
             "W" => {
                 w_horizontal_move = -number;
-            },
+            }
             "L" => {
                 angle = -number as f32;
-            },
+            }
             "R" => {
                 angle = number as f32;
-            },
+            }
             "F" => {
                 s_vertical_move = number * waypoint.0;
                 s_horizontal_move = number * waypoint.1;
-            },
-            _ => ()
+            }
+            _ => (),
         }
         let s = angle.to_radians().sin().round();
         let c = angle.to_radians().cos().round();
