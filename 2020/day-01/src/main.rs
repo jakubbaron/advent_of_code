26ab4ca345a56733fc070e336b53b24f81e5f704
Jakub Baron <jakubaron@gmail.com>
2020-12-13 20:07:20 +0100

    Cleaner file read

--- 2020/day-01/src/main.rs
+++ 2020/day-01/src/main.rs
@@ -1,20 +1,14 @@
-use std::fs::File;
-use std::io::{self, BufReader};
-use std::io::prelude::*;
+use std::io::{self};
 use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
-    let f = File::open("input.txt")?;
-    let f = BufReader::new(f);
-    let mut vec = Vec::new();
+    let f = "input.txt";
+    let vec: Vec<i32> = std::fs::read_to_string(f)?
+       .lines()
+       .map(|x| x.parse::<i32>().unwrap())
+       .collect();
+
     let mut numbers = HashSet::new();
-
-    for line in f.lines() {
-        let my_string = line.unwrap();
-        let my_int = my_string.parse::<i32>().unwrap();
-        vec.push(my_int);
-    }
-
     for my_int in &vec {
         if numbers.contains(my_int) {
             println!("{}", my_int * (2020 - my_int));
