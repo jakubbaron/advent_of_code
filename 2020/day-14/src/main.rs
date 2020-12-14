d1467843a15f7a7daf3fec1faaac53863c156247
Jakub Baron <jakubaron@gmail.com>
2020-12-14 12:44:24 +0100

    A bit of refactoring

--- 2020/day-14/src/main.rs
+++ 2020/day-14/src/main.rs
@@ -2,10 +2,32 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::io::{self};
 
-#[derive(Debug, Clone)]
-struct Mask {
+trait Mask {
+    fn apply_mask(&self, number: u64) -> u64;
+}
+
+struct MaskV1 {
     mask: u64,
     set: bool,
+}
+
+impl Mask for MaskV1 {
+    fn apply_mask(&self, number: u64) -> u64 {
+        if self.set {
+            return number | self.mask;
+        } else {
+            return number & !self.mask;
+        }
+    }
+}
+
+impl Mask for MaskV2 {
+    fn apply_mask(&self, number: u64) -> u64 {
+        if self.set {
+            return number | self.mask;
+        }
+        number
+    }
 }
 
 struct MaskV2 {
@@ -14,19 +36,39 @@ struct MaskV2 {
     float: bool,
 }
 
-fn parse_mask(mask: &str) -> Vec<Mask> {
-    let mut result: Vec<Mask> = Vec::new();
+enum MaskKind {
+    MaskV1,
+    MaskV2,
+}
+
+impl MaskV2 {
+    fn to_masks_v1(&self) -> (MaskV1, MaskV1) {
+        (
+            MaskV1 {
+                mask: self.mask,
+                set: self.set,
+            },
+            MaskV1 {
+                mask: self.mask,
+                set: !self.set,
+            },
+        )
+    }
+}
+
+fn parse_masks_v1(mask: &str) -> Vec<MaskV1> {
+    let mut result: Vec<MaskV1> = Vec::new();
     for (i, ch) in mask.chars().rev().enumerate() {
         if ch == 'X' {
             continue;
         }
         let set = ch == '1';
-        result.push(Mask { mask: 1 << i, set });
+        result.push(MaskV1 { mask: 1 << i, set });
     }
     result
 }
 
-fn parse_mask_v2(mask: &str) -> Vec<MaskV2> {
+fn parse_masks_v2(mask: &str) -> Vec<MaskV2> {
     let mut result: Vec<MaskV2> = Vec::new();
     for (i, ch) in mask.chars().rev().enumerate() {
         let float = ch == 'X';
@@ -40,62 +82,25 @@ fn parse_mask_v2(mask: &str) -> Vec<MaskV2> {
     result
 }
 
-fn apply_single_mask(number: u64, mask: &Mask) -> u64 {
-    if mask.set {
-        return number | mask.mask;
-    } else {
-        return number & !mask.mask;
-    }
+fn apply_masks_v1(number: u64, masks: &Vec<MaskV1>) -> u64 {
+    masks.iter().fold(number, |acc, mask| mask.apply_mask(acc))
 }
 
-fn apply_masks_1(number: u64, masks: &Vec<Mask>) -> u64 {
-    let mut tmp = number;
-    for mask in masks.iter() {
-        tmp = apply_single_mask(tmp, mask);
-    }
-    tmp
+fn apply_masks_v2(number: u64, masks: &Vec<MaskV2>) -> u64 {
+    masks.iter().fold(number, |acc, mask| mask.apply_mask(acc))
 }
 
-fn apply_single_mask_v2(number: u64, masks: &Vec<MaskV2>) -> u64 {
-    let mut tmp = number;
-    for mask in masks.iter() {
-        if mask.float {
-            continue;
-        }
-        if mask.set {
-            tmp = tmp | mask.mask;
-        }
-    }
-    tmp
-}
-
-fn apply_masks_2(number: u64, masks: &Vec<MaskV2>) -> Vec<u64> {
-    let mut floats: Vec<u64> = vec![apply_single_mask_v2(number, masks)];
+fn generate_addresses(number: u64, masks: &Vec<MaskV2>) -> Vec<u64> {
+    let mut floats: Vec<u64> = vec![apply_masks_v2(number, masks)];
     for iter_mask in masks.iter() {
         if !iter_mask.float {
             continue;
         }
-        let MaskV2 {
-            float: _,
-            mask,
-            set: _,
-        } = iter_mask;
+        let (m1, m2) = iter_mask.to_masks_v1();
         let mut tmp = Vec::new();
         for n in floats {
-            tmp.push(apply_single_mask(
-                n,
-                &Mask {
-                    mask: *mask,
-                    set: true,
-                },
-            ));
-            tmp.push(apply_single_mask(
-                n,
-                &Mask {
-                    mask: *mask,
-                    set: false,
-                },
-            ));
+            tmp.push(m1.apply_mask(n));
+            tmp.push(m2.apply_mask(n));
         }
         floats = tmp.to_vec();
     }
@@ -119,14 +124,14 @@ fn main() -> io::Result<()> {
             .collect();
         let mut mem_values_1: HashMap<u64, u64> = HashMap::new();
         let mut mem_values_2: HashMap<u64, u64> = HashMap::new();
-        let mut mask = parse_mask(&"X".repeat(36));
-        let mut maskv2 = parse_mask_v2(&"0".repeat(36));
+        let mut mask = Vec::new();
+        let mut maskv2 = Vec::new();
         for line in vec.iter() {
             if re_mask.is_match(&line) {
                 let caps = re_mask.captures(&line).unwrap();
                 let tmp = caps.get(1).map_or("", |m| m.as_str());
-                mask = parse_mask(&tmp);
-                maskv2 = parse_mask_v2(&tmp);
+                mask = parse_masks_v1(&tmp);
+                maskv2 = parse_masks_v2(&tmp);
                 continue;
             }
             if !re_mem.is_match(&line) {
@@ -143,9 +148,10 @@ fn main() -> io::Result<()> {
                 .map_or("0", |m| m.as_str())
                 .parse::<u64>()
                 .unwrap();
-            let value_after_mask = apply_masks_1(value, &mask);
-            mem_values_1.insert(mem_address, value_after_mask);
-            for mem_addr_2 in apply_masks_2(mem_address, &maskv2) {
+
+            mem_values_1.insert(mem_address, apply_masks_v1(value, &mask));
+
+            for mem_addr_2 in generate_addresses(mem_address, &maskv2) {
                 mem_values_2.insert(mem_addr_2, value);
             }
         }
