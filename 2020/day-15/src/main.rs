8eb098c8a73d511f6886faa7d769dba6ea6ba866
Jakub Baron <jakubaron@gmail.com>
2020-12-15 13:50:56 +0100

    Day 15, tracking only first val

--- 2020/day-15/src/main.rs
+++ 2020/day-15/src/main.rs
@@ -6,62 +6,80 @@ struct InputResult {
     result_2: usize,
 }
 
-struct LastSpoken {
-    zero:usize,
-    one:usize,
-}
-
-impl LastSpoken {
-    fn new() -> LastSpoken {
-        LastSpoken{zero:0, one:0}
-    }
-    fn is_new(&self) -> bool {
-        self.zero == 0
-    }
-    fn push(&mut self, turn: usize) {
-        self.zero = self.one;
-        self.one = turn;
-    }
-    fn diff(&self) -> usize {
-        self.one - self.zero
-    }
-}
-
 fn main() {
     let inputs = vec![
-        InputResult{input: vec![0,3,6], result_1: 436, result_2:175594},
-        InputResult{input: vec![1,3,2], result_1: 1, result_2:2578},
-        InputResult{input: vec![2,1,3], result_1: 10, result_2:3544142},
-        InputResult{input: vec![1,2,3], result_1: 27, result_2:261214},
-        InputResult{input: vec![2,3,1], result_1: 78, result_2:6895259},
-        InputResult{input: vec![3,2,1], result_1: 438, result_2:18},
-        InputResult{input: vec![3,1,2], result_1: 1836, result_2:362},
-        InputResult{input: vec![14,8,16,0,1,17], result_1: 240, result_2:505},
+        InputResult {
+            input: vec![0, 3, 6],
+            result_1: 436,
+            result_2: 175594,
+        },
+        InputResult {
+            input: vec![1, 3, 2],
+            result_1: 1,
+            result_2: 2578,
+        },
+        InputResult {
+            input: vec![2, 1, 3],
+            result_1: 10,
+            result_2: 3544142,
+        },
+        InputResult {
+            input: vec![1, 2, 3],
+            result_1: 27,
+            result_2: 261214,
+        },
+        InputResult {
+            input: vec![2, 3, 1],
+            result_1: 78,
+            result_2: 6895259,
+        },
+        InputResult {
+            input: vec![3, 2, 1],
+            result_1: 438,
+            result_2: 18,
+        },
+        InputResult {
+            input: vec![3, 1, 2],
+            result_1: 1836,
+            result_2: 362,
+        },
+        InputResult {
+            input: vec![14, 8, 16, 0, 1, 17],
+            result_1: 240,
+            result_2: 505,
+        },
     ];
+    let run_size = 30000000;
     for ir in inputs.iter() {
-        let InputResult {input, result_1, result_2 } = ir;
+        let InputResult {
+            input,
+            result_1,
+            result_2,
+        } = ir;
         println!("{:?}", input);
-        let mut tracker: HashMap<usize, LastSpoken> = HashMap::new();
-        for (i, val) in input.iter().enumerate() {
-            let mut l = LastSpoken::new();
-            l.push(i+1);
-            tracker.insert(*val, l);
+        let mut tracker: HashMap<usize, usize> = HashMap::new();
+        // let mut vec_try = vec![0_usize, run_size];
+        let (last_spoken, elements) = input.split_last().unwrap();
+        for (i, val) in elements.iter().enumerate() {
+            tracker.insert(*val, i + 1);
         }
-        let mut last_spoken = *input.last().unwrap();
-        for turn in (input.len() + 1)..30000001 {
-            tracker.entry(last_spoken).or_insert_with(LastSpoken::new);
-            let last_tracker = tracker.get(&last_spoken).unwrap();
-
-            if last_tracker.is_new() {
-                last_spoken = 0;
-            } else {
-                last_spoken = last_tracker.diff();
+        let mut last_spoken = *last_spoken;
+        for turn in input.len()..run_size {
+            match tracker.get(&last_spoken) {
+                Some(val) => {
+                    let tmp = last_spoken;
+                    last_spoken = turn - val;
+                    tracker.insert(tmp, turn);
+                }
+                None => {
+                    tracker.insert(last_spoken, turn);
+                    last_spoken = 0;
+                }
             }
-            tracker.entry(last_spoken).or_insert_with(LastSpoken::new).push(turn);
-            if turn == 2020 {
+            if turn == 2020 - 1 {
                 assert_eq!(last_spoken, *result_1);
             }
-            if turn == 30000000 {
+            if turn == 30000000 - 1 {
                 assert_eq!(last_spoken, *result_2);
             }
         }
