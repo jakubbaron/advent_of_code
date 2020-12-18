01e5ba6b1bc47735a8b1ae9834b05cafbdc805b6
Jakub Baron <jakubaron@gmail.com>
2020-12-18 22:26:55 +0100

    While loop in 3-sum

--- 2020/day-01/src/main.rs
+++ 2020/day-01/src/main.rs
@@ -11,19 +11,33 @@ fn main() -> io::Result<()> {
     let mut numbers = HashSet::new();
     for my_int in &vec {
         if numbers.contains(my_int) {
+            assert_eq!(1018336, my_int * (2020 - my_int));
             println!("{}", my_int * (2020 - my_int));
             break;
         }
         numbers.insert(2020 - my_int);
     }
-
-    for int1 in &vec {
-        for int2 in &vec {
-            for int3 in &vec {
-                if int1 + int2 + int3 == 2020 {
-                    println!("{}", int1 * int2 * int3);
-                    return Ok(());
-                }
+    let mut vec = vec;
+    vec.sort();
+    let target_sum = 2020;
+    for (i, el) in vec.iter().enumerate() {
+        let mut right = vec.len() - 1;
+        let mut left = i + 1;
+        while left < right {
+            let right_el = vec[right];
+            let left_el = vec[left];
+            let summed = el + left_el + right_el;
+            if summed == target_sum {
+                assert_eq!(288756720, right_el * left_el * el);
+                println!("{}", right_el * left_el * el);
+                break;
+            } else if summed < target_sum {
+                left += 1;
+            } else if summed > target_sum {
+                right -= 1;
+            } else {
+                println!("NOPE");
+                break;
             }
         }
     }
