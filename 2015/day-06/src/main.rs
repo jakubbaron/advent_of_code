5f75fba5e3668a7509d44e92c30848694010cc31
Jakub Baron <jakubaron@gmail.com>
2020-12-26 16:05:28 +0100

    rustfmt

--- 2015/day-06/src/main.rs
+++ 2015/day-06/src/main.rs
@@ -1,14 +1,18 @@
-use std::io::{self};
 use regex::Regex;
 use std::cmp::max;
+use std::io::{self};
 
 fn main() -> io::Result<()> {
     let files_results = vec![
         ("test.txt", 1_000 * 1_000, 1_000 * 1_000),
         ("test2.txt", 1_000, 2_000),
         ("test3.txt", 0, 0),
-        ("test4.txt", 1_000 * 1_000 - 1_000 - 4, 1_000 * 1_000 + 2_000 - 4),
-        ("input.txt", 569999, 17836115)
+        (
+            "test4.txt",
+            1_000 * 1_000 - 1_000 - 4,
+            1_000 * 1_000 + 2_000 - 4,
+        ),
+        ("input.txt", 569999, 17836115),
     ];
     for (f, result_1, result_2) in files_results.into_iter() {
         println!("File: {}", f);
@@ -22,10 +26,26 @@ fn main() -> io::Result<()> {
         for line in file_content.iter() {
             let caps = re.captures(&line).unwrap();
             let command = caps.get(1).map_or("", |m| m.as_str()).to_string();
-            let start_x = caps.get(2).map_or("", |m| m.as_str()).parse::<usize>().unwrap();
-            let start_y = caps.get(3).map_or("", |m| m.as_str()).parse::<usize>().unwrap();
-            let end_x = caps.get(4).map_or("", |m| m.as_str()).parse::<usize>().unwrap();
-            let end_y = caps.get(5).map_or("", |m| m.as_str()).parse::<usize>().unwrap();
+            let start_x = caps
+                .get(2)
+                .map_or("", |m| m.as_str())
+                .parse::<usize>()
+                .unwrap();
+            let start_y = caps
+                .get(3)
+                .map_or("", |m| m.as_str())
+                .parse::<usize>()
+                .unwrap();
+            let end_x = caps
+                .get(4)
+                .map_or("", |m| m.as_str())
+                .parse::<usize>()
+                .unwrap();
+            let end_y = caps
+                .get(5)
+                .map_or("", |m| m.as_str())
+                .parse::<usize>()
+                .unwrap();
             for x in start_x..=end_x {
                 for y in start_y..=end_y {
                     if command == "turn on" {
@@ -43,8 +63,21 @@ fn main() -> io::Result<()> {
                 }
             }
         }
-        assert_eq!(lights.iter().flat_map(|row| row.iter()).filter(|&&light| light).count(), result_1);
-        assert_eq!(brightness.iter().flat_map(|row| row.iter()).fold(0, |acc, val| acc + val), result_2);
+        assert_eq!(
+            lights
+                .iter()
+                .flat_map(|row| row.iter())
+                .filter(|&&light| light)
+                .count(),
+            result_1
+        );
+        assert_eq!(
+            brightness
+                .iter()
+                .flat_map(|row| row.iter())
+                .fold(0, |acc, val| acc + val),
+            result_2
+        );
     }
     Ok(())
 }
