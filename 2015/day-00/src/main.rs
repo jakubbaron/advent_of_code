94eee9d88efef0e5c0509db0a522fb5f965aa9e5
Jakub Baron <jakubaron@gmail.com>
2020-12-26 11:56:16 +0100

    Day 0 as a template

---
+++ 2015/day-00/src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self};
+
+fn main() -> io::Result<()> {
+    let files_results = vec![
+        ("test.txt", 1, 1),
+        ("input.txt", 1, 1)
+    ];
+    for (f, result_1, _result_2) in files_results.into_iter() {
+        println!("File: {}", f);
+        let file_content: Vec<&str> = std::fs::read_to_string(f)?
+            .lines()
+            .map(|x| x.to_string())
+            .collect();
+        for line in file_content.iter() {
+
+        }
+    }
+    Ok(())
+}
