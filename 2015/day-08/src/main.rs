b6021201f76b437b922be9aacf7979bb7096c2a6
Jakub Baron <jakubaron@gmail.com>
2020-12-27 19:49:32 +0100

    Day 8, both parts

--- 2015/day-08/src/main.rs
+++ 2015/day-08/src/main.rs
@@ -1,36 +1,40 @@
 use std::io::{self};
-// use regex::Regex;
+
+fn get_result_1(file_content: &Vec<String>) -> usize {
+    let mut total_len = 0;
+    let mut total_mem_len = 0;
+    for line in file_content.iter() {
+        total_mem_len += line.len();
+        let new_line = line
+            .replace("\\\\", "#")
+            .replace("\\\"", "#")
+            .replace("\"", "");
+        let len_modifier = new_line.matches("\\x").count() * 3; // from 4 chars -> 1 char
+        total_len += (new_line.len() - len_modifier);
+    }
+    total_mem_len - total_len
+}
+
+fn get_result_2(file_content: &Vec<String>) -> usize {
+    let mut total_len = 0;
+    let mut super_len = 0;
+    for line in file_content.iter() {
+        total_len += line.len();
+        super_len += line.len() + line.matches("\\").count() + line.matches("\"").count() + 2 /* two quotes */;
+    }
+    super_len - total_len
+}
 
 fn main() -> io::Result<()> {
-    let files_results = vec![
-        ("test.txt", 12, 1),
-        ("input.txt", 1387, 1)
-    ];
+    let files_results = vec![("test.txt", 12, 19), ("input.txt", 1371, 1)];
     for (f, result_1, result_2) in files_results.into_iter() {
         println!("File: {}", f);
         let file_content: Vec<String> = std::fs::read_to_string(f)?
             .lines()
             .map(|x| x.to_string())
             .collect();
-        let mut total_len = 0;
-        let mut total_mem_len = 0;
-        for line in file_content.iter() {
-            total_mem_len += line.len();
-            let mut len_modifier = 2;
-            len_modifier += line.matches("\\\\").count();
-            len_modifier += line.matches("\\x").count() * 3; // from 4 chars -> 1 char
-            len_modifier += line.matches("\\\"").count();
-            total_len += (line.len() - len_modifier);
-            println!(
-                "Line {}, line.len() {}, len_modfier: {}, subtracted: {}",
-                line,
-                line.len(),
-                len_modifier,
-                line.len() - len_modifier,
-            );
-        }
-        let res_1 = total_mem_len - total_len;
-        assert_eq!(res_1, result_1);
+        assert_eq!(get_result_1(&file_content), result_1);
+        assert_eq!(get_result_2(&file_content), result_2);
     }
     Ok(())
 }
