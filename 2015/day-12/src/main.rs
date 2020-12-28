aec57eea3574ee28011987a028b373a261662fa8
Jakub Baron <jakubaron@gmail.com>
2020-12-28 14:46:47 +0100

    Day 12, both parts with serde_json

--- 2015/day-12/src/main.rs
+++ 2015/day-12/src/main.rs
@@ -2,12 +2,39 @@ use std::io::{self};
 use regex::Regex;
 use std::cmp::{max, min};
 use std::collections::HashSet;
+use serde_json::{Result, Value};
 
 fn count_numbers(line: &str) -> i32 {
     let re = Regex::new(r"([-]?\d+)").unwrap();
     re.find_iter(&line).map(|c| {
         c.as_str().parse::<i32>().unwrap()
     }).fold(0, |acc, val| acc + val)
+}
+
+fn counter(vals: &Value) -> i64 {
+    match vals {
+        Value::Object(map) => {
+            for (k, v) in map {
+                if v == &Value::String("red".to_string()) {
+                    return 0;
+                }
+            }
+            let mut sum = 0;
+            for (k, v) in map {
+                sum += counter(v);
+            }
+            return sum;
+        },
+        Value::Array(array) => {
+            return array.iter().map(|v| counter(v)).fold(0, |acc, val| acc + val);
+        },
+        Value::Number(n) => {
+            return n.as_i64().unwrap();
+        },
+        _ => {
+            return 0;
+        },
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -24,75 +51,11 @@ fn main() -> io::Result<()> {
             .map(|x| x.to_string())
             .collect();
         let line = &file_content[0];
-        // assert_eq!(count_numbers(&line), result_1);
+        assert_eq!(count_numbers(&line), result_1);
 
-        let chars: Vec<char> = line.chars().collect();
-        let mut to_remove: Vec<(usize, usize)> = Vec::new();
-        let mut openings: Vec<usize> = Vec::new();
-        let mut id = 0;
-        while id < chars.len() {
-            if chars[id] == '{' {
-                openings.push(id);
-            } else if chars[id] == '}' {
-                to_remove.push((openings.pop().unwrap(), id));
-            }
-            id += 1;
-        }
-        let red_str = "\":\"red\"";
-        let mut having_red: Vec<(usize, usize)> = Vec::new();
-        for (open, close) in to_remove.iter() {
-            let slice = &line[*open..*close+1];
-            if !slice.contains(red_str) {
-                continue;
-            }
+        let v: Value = serde_json::from_str(line).unwrap();
+        assert_eq!(counter(&v), result_2);
 
-            let mut replace_index: Option<usize> = None;
-            for (id, (o2, c2)) in having_red.iter().enumerate() {
-                if max(close, c2) - min(open, o2) < (close - open) + (c2 - o2) {
-                    let mut all_sets: HashSet<usize> = HashSet::new();
-                    for (o, c) in having_red.iter() {
-                        if o == o2 && c == c2 {
-                            continue;
-                        }
-                        let prev: HashSet<_> = line[*o..*c+1].match_indices(red_str).map(|(val, _)| val+*o).collect();
-                        all_sets = all_sets.union(&prev).cloned().collect();
-                    }
-                    let v: HashSet<_> = slice.match_indices(red_str).map(|(val, _)| val + *open).collect();
-                    let previous: HashSet<_> = line[*o2..*c2+1].match_indices(red_str).map(|(val, _)| val+*o2).collect();
-                    if v.is_superset(&previous) && v.is_disjoint(&all_sets) {
-                        replace_index = Some(id);
-                        break;
-                    }
-                }
-            }
-            if let Some(i) = replace_index {
-                println!("Replacing {:?} with ({}, {})", having_red[i], open, close);
-                having_red[i] = (*open, *close);
-            } else {
-                let v: HashSet<_> = slice.match_indices(red_str).map(|(val, _)| val + *open).collect();
-                let mut all_sets: HashSet<usize> = HashSet::new();
-                for (o, c) in having_red.iter() {
-                    let previous: HashSet<_> = line[*o..*c+1].match_indices(red_str).map(|(val, _)| val+*o).collect();
-                    all_sets = all_sets.union(&previous).cloned().collect();
-                }
-                if v.is_disjoint(&all_sets) {
-                    println!("Pushing {} {}", open, close);
-                    having_red.push((*open, *close));
-                }
-            }
-        }
-        println!("{:?}", having_red);
-
-        let mut red_sum = 0;
-        for (open, close) in having_red.iter() {
-            red_sum += count_numbers(&line[*open..*close+1]);
-            // println!("{}", &line[*open..*close+1]);
-        }
-
-        println!("Red sum: {}", red_sum);
-        let s = count_numbers(&line) - red_sum;
-        println!("Sum: {}", s);
-        assert_eq!(s, result_2);
     }
     Ok(())
 }
