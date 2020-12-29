807d45a58e33daac5cd38060b1da38c14f19f1ea
Jakub Baron <jakubaron@gmail.com>
2020-12-29 21:49:24 +0100

    Remove two loops in favour of fold

--- 2015/day-15/src/lib.rs
+++ 2015/day-15/src/lib.rs
@@ -47,6 +47,8 @@ fn get_score(vec: &Vec<i64>) -> i64 {
 fn add_spoon(vec: &Vec<i64>, spoon: &Spoon) -> Vec<i64> {
     let mut copy = vec.to_vec();
     let spoon_vec = spoon.as_vec();
+    assert!(copy.len() >= spoon_vec.len());
+
     for i in 0..copy.len() {
         copy[i] += spoon_vec[i];
     }
@@ -54,18 +56,18 @@ fn add_spoon(vec: &Vec<i64>, spoon: &Spoon) -> Vec<i64> {
 }
 
 pub fn optimize_for_score(spoons_vec: &Vec<Spoon>, iterations: usize) -> i64 {
-    assert!(spoons_vec.len() > 0);
-    assert!(spoons_vec.len() < iterations);
+    let spoons_len = spoons_vec.len();
+    assert!(spoons_len > 0);
+    assert!(spoons_len < iterations);
 
-    let mut scores: Vec<i64> = vec![0; spoons_vec[0].as_vec().len()];
-    let mut used_spoons = 0;
-    for spoon in spoons_vec.iter() {
-        scores = add_spoon(&scores, &spoon);
-        used_spoons += 1;
-    }
-
+    let spoon_prop_len = spoons_vec[0].as_vec().len();
+    let mut scores = spoons_vec
+        .iter()
+        .fold(vec![0; spoon_prop_len], |scores, spoon| {
+            add_spoon(&scores, &spoon)
+        });
     let mut res_1 = 0;
-    for _ in used_spoons..iterations {
+    for _ in spoons_len..iterations {
         let (score, new_scores) = spoons_vec
             .iter()
             .map(|spoon| add_spoon(&scores, &spoon))
@@ -83,29 +85,34 @@ pub fn optimize_for_score_and_kcals(
     required_kcals: usize,
     iterations: usize,
 ) -> i64 {
-    assert!(spoons_vec.len() > 0);
-    assert!(spoons_vec.len() < iterations);
-    let mut scores: Vec<i64> = vec![0; spoons_vec[0].as_vec().len()];
-    let mut used_spoons = 0;
-    let mut used_kcals = 0;
-    for spoon in spoons_vec.iter() {
-        scores = add_spoon(&scores, &spoon);
-        used_spoons += 1;
-        used_kcals += spoon.get_calories();
-    }
+    let spoons_len = spoons_vec.len();
+    assert!(spoons_len > 0);
+    assert!(spoons_len < iterations);
+
+    let used_kcals = spoons_vec
+        .iter()
+        .fold(0, |acc, spoon| acc + spoon.get_calories());
     assert!(used_kcals <= required_kcals);
 
-    let mut calories_scores = vec![vec![(0, vec![0]); required_kcals + 1]; iterations + 1];
-    calories_scores[used_spoons][used_kcals] = (get_score(&scores), scores);
-    for spoons in used_spoons + 1..iterations + 1 {
-        for kcals in used_kcals + 1..required_kcals + 1 {
+    let spoon_prop_len = spoons_vec[0].as_vec().len();
+    let scores = spoons_vec
+        .iter()
+        .fold(vec![0; spoon_prop_len], |scores, spoon| {
+            add_spoon(&scores, &spoon)
+        });
+
+    let mut calories_scores =
+        vec![vec![vec![0; spoon_prop_len]; required_kcals + 1]; iterations + 1];
+    calories_scores[spoons_len][used_kcals] = scores;
+    for spoons in spoons_len + 1..=iterations {
+        for kcals in used_kcals + 1..=required_kcals {
             let mut max_score = 0;
             let mut max_vec: Vec<i64> = Vec::new();
             for spoon in spoons_vec.iter() {
                 if spoon.get_calories() >= kcals {
                     continue;
                 } else {
-                    let (_, scores) = &calories_scores[spoons - 1][kcals - spoon.get_calories()];
+                    let scores = &calories_scores[spoons - 1][kcals - spoon.get_calories()];
                     let tmp = add_spoon(&scores, &spoon);
                     let tmp_result = get_score(&tmp);
                     if tmp_result > max_score {
@@ -114,11 +121,11 @@ pub fn optimize_for_score_and_kcals(
                     }
                 }
             }
-            calories_scores[spoons][kcals] = (max_score, max_vec);
+            calories_scores[spoons][kcals] = max_vec;
         }
     }
     // println!("[{}][{}]: {:?}", 100, 500, calories_scores[100][500]);
-    calories_scores[iterations][required_kcals].0
+    get_score(&calories_scores[iterations][required_kcals])
 }
 
 #[cfg(test)]
