db2acea98bfe490a3b0f14752da2f03a08ebe834
Jakub Baron <jakubaron@gmail.com>
2020-12-29 19:16:09 +0100

    Move part 1 and part 2 to lib.rs

--- 2015/day-15/src/lib.rs
+++ 2015/day-15/src/lib.rs
@@ -33,7 +33,7 @@ impl Spoon {
     }
 }
 
-pub fn get_score(vec: &Vec<i64>) -> i64 {
+fn get_score(vec: &Vec<i64>) -> i64 {
     let mut score = 1;
     for el in vec {
         if *el < 0 {
@@ -44,13 +44,81 @@ pub fn get_score(vec: &Vec<i64>) -> i64 {
     score
 }
 
-pub fn add_spoon(vec: &Vec<i64>, spoon: &Spoon) -> Vec<i64> {
+fn add_spoon(vec: &Vec<i64>, spoon: &Spoon) -> Vec<i64> {
     let mut copy = vec.to_vec();
     let spoon_vec = spoon.as_vec();
     for i in 0..copy.len() {
         copy[i] += spoon_vec[i];
     }
     copy
+}
+
+pub fn optimize_for_score(spoons_vec: &Vec<Spoon>, iterations: usize) -> i64 {
+    assert!(spoons_vec.len() > 0);
+    assert!(spoons_vec.len() < iterations);
+
+    let mut scores: Vec<i64> = vec![0; spoons_vec[0].as_vec().len()];
+    let mut used_spoons = 0;
+    for spoon in spoons_vec.iter() {
+        scores = add_spoon(&scores, &spoon);
+        used_spoons += 1;
+    }
+
+    let mut res_1 = 0;
+    for _ in used_spoons..iterations {
+        let (score, new_scores) = spoons_vec
+            .iter()
+            .map(|spoon| add_spoon(&scores, &spoon))
+            .map(|new_scores| (get_score(&new_scores), new_scores))
+            .max()
+            .unwrap();
+        scores = new_scores;
+        res_1 = score;
+    }
+    res_1
+}
+
+pub fn optimize_for_score_and_kcals(
+    spoons_vec: &Vec<Spoon>,
+    required_kcals: usize,
+    iterations: usize,
+) -> i64 {
+    assert!(spoons_vec.len() > 0);
+    assert!(spoons_vec.len() < iterations);
+    let mut scores: Vec<i64> = vec![0; spoons_vec[0].as_vec().len()];
+    let mut used_spoons = 0;
+    let mut used_kcals = 0;
+    for spoon in spoons_vec.iter() {
+        scores = add_spoon(&scores, &spoon);
+        used_spoons += 1;
+        used_kcals += spoon.get_calories();
+    }
+    assert!(used_kcals <= required_kcals);
+
+    let mut calories_scores = vec![vec![(0, vec![0]); required_kcals + 1]; iterations + 1];
+    calories_scores[used_spoons][used_kcals] = (get_score(&scores), scores);
+    for spoons in used_spoons + 1..iterations + 1 {
+        for kcals in used_kcals + 1..required_kcals + 1 {
+            let mut max_score = 0;
+            let mut max_vec: Vec<i64> = Vec::new();
+            for spoon in spoons_vec.iter() {
+                if spoon.get_calories() >= kcals {
+                    continue;
+                } else {
+                    let (_, scores) = &calories_scores[spoons - 1][kcals - spoon.get_calories()];
+                    let tmp = add_spoon(&scores, &spoon);
+                    let tmp_result = get_score(&tmp);
+                    if tmp_result > max_score {
+                        max_score = tmp_result;
+                        max_vec = tmp;
+                    }
+                }
+            }
+            calories_scores[spoons][kcals] = (max_score, max_vec);
+        }
+    }
+    // println!("[{}][{}]: {:?}", 100, 500, calories_scores[100][500]);
+    calories_scores[iterations][required_kcals].0
 }
 
 #[cfg(test)]
--- 2015/day-15/src/main.rs
+++ 2015/day-15/src/main.rs
@@ -1,4 +1,4 @@
-use day_15::{add_spoon, get_score, Spoon};
+use day_15::{optimize_for_score, optimize_for_score_and_kcals, Spoon};
 use regex::Regex;
 use std::io::{self};
 
@@ -48,64 +48,11 @@ fn main() -> io::Result<()> {
             ));
         }
 
-        let mut scores: Vec<i64> = vec![0; spoons_vec[0].as_vec().len()];
-        let mut used_spoons = 0;
-        for spoon in spoons_vec.iter() {
-            scores = add_spoon(&scores, &spoon);
-            used_spoons += 1;
-        }
-
-        // println!("Score: {}", get_score(&scores));
-        // println!("Scores: {:?}", scores);
-        // println!("Used Spoons: {:?}", used_spoons);
-
-        let mut res_1 = 0;
-        for _ in used_spoons..100 {
-            let (score, new_scores) = spoons_vec
-                .iter()
-                .map(|spoon| add_spoon(&scores, &spoon))
-                .map(|new_scores| (get_score(&new_scores), new_scores))
-                .max()
-                .unwrap();
-            scores = new_scores;
-            res_1 = score;
-        }
-        assert_eq!(res_1, result_1);
-        let mut calories_scores = vec![vec![(0, vec![0]); 501]; 101];
-        let mut scores: Vec<i64> = vec![0; spoons_vec[0].as_vec().len()];
-        let mut used_spoons = 0;
-        let mut used_kcals = 0;
-        for spoon in spoons_vec.iter() {
-            scores = add_spoon(&scores, &spoon);
-            used_spoons += 1;
-            used_kcals += spoon.get_calories();
-        }
-        calories_scores[used_spoons][used_kcals] = (get_score(&scores), scores);
-        for spoons in used_spoons + 1..101 {
-            for kcals in used_kcals + 1..501 {
-                let mut max_score = 0;
-                let mut max_vec: Vec<i64> = Vec::new();
-                for spoon in spoons_vec.iter() {
-                    if spoon.get_calories() >= kcals {
-                        continue;
-                    } else {
-                        let (_, scores) =
-                            &calories_scores[spoons - 1][kcals - spoon.get_calories()];
-                        let tmp = add_spoon(&scores, &spoon);
-                        let tmp_result = get_score(&tmp);
-                        if tmp_result > max_score {
-                            max_score = tmp_result;
-                            max_vec = tmp;
-                        }
-                    }
-                }
-                calories_scores[spoons][kcals] = (max_score, max_vec);
-                // println!("[{}][{}]: {:?}", spoons, kcals, calories_scores[spoons][kcals]);
-            }
-        }
-        println!("[{}][{}]: {:?}", 100, 500, calories_scores[100][500]);
-        let res_2 = calories_scores[100][500].0;
-        assert_eq!(res_2, result_2);
+        assert_eq!(optimize_for_score(&spoons_vec, 100), result_1);
+        assert_eq!(
+            optimize_for_score_and_kcals(&spoons_vec, 500, 100),
+            result_2
+        );
     }
     Ok(())
 }
