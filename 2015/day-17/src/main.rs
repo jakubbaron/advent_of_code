1e7fcb08db6436cd029621f2fb570fd384ed2930
Jakub Baron <jakubaron@gmail.com>
2020-12-30 12:08:19 +0100

    Day 17 with recursion

--- 2015/day-17/src/main.rs
+++ 2015/day-17/src/main.rs
@@ -3,10 +3,10 @@ use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
     let files_results = vec![
-        ("test.txt", 25, 4, 3),
-        ("input.txt", 150, 654, 57)
+        ("test.txt", 25, 4, 2, 3), // 2 is from part 1, min number of bottles
+        ("input.txt", 150, 654, 4, 57) // 4 is from part 1, min number of bottles
     ];
-    for (f, capacity, result_1, result_2) in files_results.into_iter() {
+    for (f, capacity, result_1, no_of_bottles, result_2) in files_results.into_iter() {
         println!("File: {}", f);
         let file_content: Vec<String> = std::fs::read_to_string(f)?
             .lines()
@@ -15,7 +15,7 @@ fn main() -> io::Result<()> {
         let containers: Vec<usize> = file_content.into_iter().map(|x| x.parse::<usize>().unwrap()).collect();
         let mut res_1 = 0;
         let mut m: HashMap<usize, usize> = HashMap::new();
-        for i in 0..1<<containers.len() {
+        for i in 0..1 << containers.len() {
             let mut t = i;
             let mut s = 0;
             let mut p = 0;
@@ -34,6 +34,40 @@ fn main() -> io::Result<()> {
         assert_eq!(res_1, result_1);
         let min_key = m.keys().min().unwrap();
         assert_eq!(*m.get(min_key).unwrap(), result_2);
+
+        fn counts(required: usize, containers: &[usize]) -> usize {
+            if required == 0 {
+                return 1;
+            }
+            if containers.len() == 0 {
+                return 0;
+            }
+            let first = *&containers[0];
+            let rest = &containers[1..];
+            if first > required {
+                counts(required, &rest)
+            } else {
+                counts(required - first, &rest) + counts(required, &rest)
+            }
+        }
+
+        assert_eq!(counts(capacity, &containers), result_1);
+        fn counts_2(required: usize, containers: &[usize], count: usize) -> usize {
+            if required == 0 {
+                return 1;
+            }
+            if containers.len() == 0 {
+                return 0;
+            }
+            let first = *&containers[0];
+            let rest = &containers[1..];
+            if first > required || count == 0  {
+                counts_2(required, &rest, count)
+            } else {
+                counts_2(required - first, &rest, count - 1) + counts_2(required, &rest, count)
+            }
+        }
+        assert_eq!(counts_2(capacity, &containers, no_of_bottles), result_2);
     }
     Ok(())
 }
