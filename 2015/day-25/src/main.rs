a974dab90941f6e81c740e33a052e948230904c5
Jakub Baron <jakubaron@gmail.com>
2021-01-04 01:12:50 +0100

    O(1) memory

--- 2015/day-25/src/main.rs
+++ 2015/day-25/src/main.rs
@@ -6,22 +6,21 @@ fn main() -> io::Result<()> {
         println!("File: {}", f);
         let sought_row = 2978;
         let sought_col = 3083;
-        let max_of_both = std::cmp::max(sought_row, sought_col) * 2;
-        let mut results = vec![vec![0_usize; max_of_both]; max_of_both];
         let mut last_val = 20151125_usize;
         let mut diagonal = 2;
-        results[1][1] = last_val;
-        while results[sought_row][sought_col] == 0 {
+        'outer: loop {
             for i in 0..diagonal {
                 let r = diagonal - i;
                 let c = i + 1;
-                results[r][c] = last_val * 252533 % 33554393;
-                last_val = results[r][c];
+                last_val = last_val * 252533 % 33554393;
+                if r == sought_row && c == sought_col {
+                    break 'outer;
+                }
             }
             diagonal += 1;
         }
-        println!("Code from the machine: {}", results[sought_row][sought_col]);
-        assert_eq!(results[sought_row][sought_col], result_1);
+        println!("Code from the machine: {}", last_val);
+        assert_eq!(last_val, result_1);
     }
     Ok(())
 }
