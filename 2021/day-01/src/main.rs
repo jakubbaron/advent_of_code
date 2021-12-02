90afc4e1522c0f6988ea216205359e7c6ce7a034
Jakub Baron <jakubaron@gmail.com>
2021-12-02 22:51:11 +0000

    day-01, methods

--- 2021/day-01/src/main.rs
+++ 2021/day-01/src/main.rs
@@ -1,5 +1,26 @@
 use std::io::{self};
 
+fn count_bigger_neighbours(container: &Vec<u32>) -> usize {
+    let mut result = 0;
+    for i in 1..container.len() {
+        if container[i] > container[i - 1] {
+            result += 1;
+        }
+    }
+    result
+}
+
+fn running_sum(container: &Vec<u32>, sum_size: usize) -> Vec<u32> {
+    let sum_1 = sum_size - 1;
+    let mut output: Vec<u32> = vec![0; container.len() - sum_1];
+    for i in sum_1..container.len() {
+        for j in 0..sum_size {
+            output[i - sum_1] += container[i - j]
+        }
+    }
+    output
+}
+
 fn main() -> io::Result<()> {
     let files_results = vec![("test.txt", 7, 5), ("input.txt", 1288, 1311)];
     for (f, result_1, result_2) in files_results.into_iter() {
@@ -8,27 +29,12 @@ fn main() -> io::Result<()> {
             .lines()
             .map(|x| x.to_string().parse::<u32>().unwrap())
             .collect();
-        let mut res_1 = 0;
-        for i in 1..file_content.len() {
-            if file_content[i] > file_content[i - 1] {
-                res_1 += 1;
-            }
-        }
+
+        let res_1 = count_bigger_neighbours(&file_content);
         assert_eq!(res_1, result_1);
 
-        let mut res_2 = 0;
-        let mut helper: Vec<u32> = vec![0; file_content.len()];
-
-        for i in 2..file_content.len() {
-            for j in 0..3 {
-                helper[i] += file_content[i - j];
-            }
-        }
-        for i in 3..helper.len() {
-            if helper[i] > helper[i - 1] {
-                res_2 += 1;
-            }
-        }
+        let helper: Vec<u32> = running_sum(&file_content, 3);
+        let res_2 = count_bigger_neighbours(&helper);
 
         assert_eq!(res_2, result_2);
     }
