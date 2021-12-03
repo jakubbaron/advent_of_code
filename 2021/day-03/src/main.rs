84bd2fa5a956fc34262894f7f9546ac907dab937
Jakub Baron <jakubaron@gmail.com>
2021-12-03 14:59:45 +0000

    day-03, both parts

---
+++ 2021/day-03/src/main.rs
@@ -0,0 +1,119 @@
+use std::io::{self};
+
+fn vec_to_dec(container: &Vec<i32>) -> i32 {
+    let helper: Vec<String> = container.iter().map(|x| x.to_string()).collect();
+    let s: String = helper.into_iter().collect();
+    i32::from_str_radix(&s, 2).unwrap()
+}
+
+fn most_significant(container: &Vec<i32>) -> Vec<i32> {
+    let mut helper = container.clone();
+    for i in 0..helper.len() {
+        if helper[i] >= 0 {
+            helper[i] = 1;
+        } else {
+            helper[i] = 0;
+        }
+    }
+    helper
+}
+
+fn least_significant(container: &Vec<i32>) -> Vec<i32> {
+    let mut helper = container.clone();
+    for i in 0..helper.len() {
+        if helper[i] >= 0 {
+            helper[i] = 0;
+        } else {
+            helper[i] = 1;
+        }
+    }
+    helper
+}
+
+fn count_collection(file_content: &Vec<String>) -> Vec<i32> {
+    let mut helper: Vec<i32> = vec![0; file_content[0].len()];
+    for line in file_content.iter() {
+        for (i, ch) in line.chars().enumerate() {
+            if ch == '1' {
+                helper[i] += 1;
+            } else {
+                helper[i] -= 1;
+            }
+        }
+    }
+    helper
+}
+
+fn part_1(file_content: &Vec<String>) -> i32 {
+    let helper = count_collection(&file_content);
+    let gamma = vec_to_dec(&most_significant(&helper));
+    let epsilon = vec_to_dec(&least_significant(&helper));
+    gamma * epsilon
+}
+
+fn part_2(file_content: &Vec<String>) -> i32 {
+    let mut current_pool = file_content.clone();
+    for ch_idx in 0..file_content[0].len() {
+        let tmp = most_significant(&count_collection(&current_pool));
+        let mut tmp_pool: Vec<String> = vec![];
+        let ch = tmp[ch_idx].to_string();
+        for line in current_pool.iter() {
+            let vec_line: Vec<String> = line.chars().map(|x| x.to_string()).collect();
+            if vec_line[ch_idx] == ch {
+                tmp_pool.push(line.clone());
+            }
+        }
+        current_pool = tmp_pool;
+        if current_pool.len() == 1 {
+            break;
+        }
+    }
+    let oxygen = vec_to_dec(
+        &current_pool[0]
+            .chars()
+            .map(|x| x.to_string().parse::<i32>().unwrap())
+            .collect(),
+    );
+    println!("{}", oxygen);
+    let mut current_pool = file_content.clone();
+    for ch_idx in 0..file_content[0].len() {
+        let tmp = least_significant(&count_collection(&current_pool));
+        let mut tmp_pool: Vec<String> = vec![];
+        let ch = tmp[ch_idx].to_string();
+        for line in current_pool.iter() {
+            let vec_line: Vec<String> = line.chars().map(|x| x.to_string()).collect();
+            if vec_line[ch_idx] == ch {
+                tmp_pool.push(line.clone());
+            }
+        }
+        current_pool = tmp_pool;
+        if current_pool.len() == 1 {
+            break;
+        }
+    }
+    let co2 = vec_to_dec(
+        &current_pool[0]
+            .chars()
+            .map(|x| x.to_string().parse::<i32>().unwrap())
+            .collect(),
+    );
+    println!("{}", co2);
+    oxygen * co2
+}
+
+fn main() -> io::Result<()> {
+    let files_results = vec![("test.txt", 198, 230), ("input.txt", 4103154, 1311)];
+    for (f, result_1, result_2) in files_results.into_iter() {
+        println!("{}", f);
+        let file_content: Vec<String> = std::fs::read_to_string(f)?
+            .lines()
+            .map(|x| x.to_string())
+            .collect();
+        let res_1 = part_1(&file_content);
+        assert_eq!(res_1, result_1);
+
+        let res_2 = part_2(&file_content);
+        assert_eq!(res_2, result_2);
+    }
+    Ok(())
+}
