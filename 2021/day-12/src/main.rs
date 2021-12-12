a94db995e15f0bdecb0fc9a75ad356e18e9b4bb0
Jakub Baron <jakubaron@gmail.com>
2021-12-12 22:20:23 +0000

    Don't count lowercase each time

--- 2021/day-12/src/main.rs
+++ 2021/day-12/src/main.rs
@@ -1,33 +1,49 @@
 use std::collections::{HashMap, HashSet};
 use std::io::{self};
 
-fn get_graph(file_content: &Vec<String>) -> HashMap<&str, Vec<&str>> {
+fn get_graph(file_content: &Vec<String>) -> HashMap<&str, Vec<Node>> {
     let mut output: HashMap<_, _> = HashMap::new();
     for line in file_content.iter() {
         let splitted: Vec<_> = line.split("-").collect::<Vec<_>>();
         let start = splitted[0];
         let end = splitted[1];
         let entry = output.entry(start).or_insert(vec![]);
-        entry.push(end);
+        entry.push(Node::new(end));
         let entry = output.entry(end).or_insert(vec![]);
-        entry.push(start);
+        entry.push(Node::new(start));
     }
     output.remove("end");
     for (_k, v) in output.iter_mut() {
-        if let Some(index) = v.iter().position(|x| *x == "start") {
+        if let Some(index) = v.iter().position(|x| x.value == "start") {
             v.remove(index);
         }
     }
     output
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+struct Node<'a> {
+    value: &'a str,
+    is_lowercase: bool,
+}
+
+impl<'a> Node<'a> {
+    fn new(value: &str) -> Node {
+        let is_lowercase = is_lowercase(value);
+        Node {
+            value,
+            is_lowercase,
+        }
+    }
+}
+
 fn is_lowercase(edge: &str) -> bool {
     edge.chars().all(|c| c.is_ascii_lowercase())
 }
 
-fn is_small_cave_visited_twice(current_path: &Vec<&str>) -> bool {
+fn is_small_cave_visited_twice(current_path: &Vec<&Node>) -> bool {
     let mut duplicates: HashSet<_> = HashSet::new();
-    for entry in current_path.iter().filter(|x| is_lowercase(x)) {
+    for entry in current_path.iter().filter(|x| x.is_lowercase) {
         if duplicates.contains(entry) {
             return true;
         }
@@ -36,10 +52,10 @@ fn is_small_cave_visited_twice(current_path: &Vec<&str>) -> bool {
     false
 }
 
-fn more_than_one_small_cave_visited_twice(current_path: &Vec<&str>) -> bool {
+fn more_than_one_small_cave_visited_twice(current_path: &Vec<&Node>) -> bool {
     let mut duplicates: HashMap<_, _> = HashMap::new();
-    for entry in current_path.iter().filter(|x| is_lowercase(x)) {
-        let entry = duplicates.entry(entry).or_insert(0);
+    for curr_el in current_path.iter().filter(|x| x.is_lowercase) {
+        let entry = duplicates.entry(curr_el).or_insert(0);
         *entry += 1;
         if *entry > 2 {
             return true;
@@ -49,23 +65,23 @@ fn more_than_one_small_cave_visited_twice(current_path: &Vec<&str>) -> bool {
 }
 
 fn count_all_paths(
-    graph: &HashMap<&str, Vec<&str>>,
-    start: &str,
-    end: &str,
-    path: &Vec<&str>,
-    method: fn(&Vec<&str>) -> bool,
+    graph: &HashMap<&str, Vec<Node>>,
+    start: &Node,
+    end: &Node,
+    path: &Vec<&Node>,
+    method: fn(&Vec<&Node>) -> bool,
 ) -> usize {
     if start == end {
         return 1;
     }
 
     let mut current_path = path.to_vec();
-    current_path.push(start);
-    if is_lowercase(start) && method(&current_path) {
+    current_path.push(&start);
+    if start.is_lowercase && method(&current_path) {
         return 0;
     }
 
-    graph[start].iter().fold(0, |acc, node| {
+    graph[start.value].iter().fold(0, |acc, node| {
         acc + count_all_paths(&graph, node, end, &current_path, method)
     })
 }
@@ -73,8 +89,8 @@ fn count_all_paths(
 fn part_1(file_content: &Vec<String>) -> usize {
     count_all_paths(
         &get_graph(&file_content),
-        "start",
-        "end",
+        &Node::new("start"),
+        &Node::new("end"),
         &Vec::new(),
         is_small_cave_visited_twice,
     )
@@ -83,8 +99,8 @@ fn part_1(file_content: &Vec<String>) -> usize {
 fn part_2(file_content: &Vec<String>) -> usize {
     count_all_paths(
         &get_graph(&file_content),
-        "start",
-        "end",
+        &Node::new("start"),
+        &Node::new("end"),
         &Vec::new(),
         more_than_one_small_cave_visited_twice,
     )
